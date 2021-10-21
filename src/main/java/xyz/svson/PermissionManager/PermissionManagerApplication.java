ea7bd21520145a8406de0cc8dc4cbff687a79cc4
svvv <svvv>
2021-10-21 19:04:52 +0300

    Barebones Spring Initializer check-in

---
+++ src/main/java/xyz/svson/PermissionManager/PermissionManagerApplication.java
@@ -0,0 +1,13 @@
+package xyz.svson.PermissionManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/svson/PermissionManager/PermissionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.svson.PermissionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
