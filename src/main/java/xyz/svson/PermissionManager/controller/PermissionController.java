4f8bf9b032ffc47bfee6a5c43d5064cfe5bf3c50
svvv <svvv>
2021-10-25 19:46:05 +0300

    Implement creating permissions

--- src/main/java/xyz/svson/PermissionManager/controller/PermissionController.java
+++ src/main/java/xyz/svson/PermissionManager/controller/PermissionController.java
@@ -1,16 +1,21 @@
 package xyz.svson.PermissionManager.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import xyz.svson.PermissionManager.model.Permission;
 import xyz.svson.PermissionManager.repository.PermissionRepository;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/permissions")
 public class PermissionController {
+    private Logger logger = LoggerFactory.getLogger(PermissionController.class);
+
     private final PermissionRepository repository;
 
     PermissionController(PermissionRepository repository) {
@@ -20,5 +25,27 @@ public class PermissionController {
     @GetMapping("/available")
     public List<Permission> list() {
         return (List<Permission>) repository.findAll();
+    }
+
+    @PostMapping
+    public Permission createPermission(@RequestBody Permission permission) {
+        if (permission == null) {
+            logger.warn("createPermission(): Empty request received");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing permission data");
+        }
+
+        if (permission.getTitle() == null) {
+            logger.warn("createPermission(): Attempted to create permission without a title");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing title");
+        }
+
+        var existingPermission = repository.findByTitle(permission.getTitle());
+        if (existingPermission.isPresent()) {
+            logger.warn("createPermission(): Attempted to create permission with non-unique title");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Non-unique title");
+        }
+
+        permission = repository.save(permission);
+        return permission;
     }
 }
--- src/main/java/xyz/svson/PermissionManager/repository/PermissionRepository.java
+++ src/main/java/xyz/svson/PermissionManager/repository/PermissionRepository.java
@@ -3,5 +3,8 @@ package xyz.svson.PermissionManager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.svson.PermissionManager.model.Permission;
 
+import java.util.Optional;
+
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    public Optional<Permission> findByTitle(String title);
 }
